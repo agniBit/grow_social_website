c4548049def76e51aa564201aa1ff80dc74fe04f
Abhishek Agnihotri <agni.abhi00@gmail.com>
2021-07-17 21:49:59 +0530

    improve responsiveness

--- src/navbar/navigationBar.js
+++ src/navbar/navigationBar.js
@@ -1,6 +1,6 @@
 import React, { useState, useEffect } from 'react';
 import { Link } from 'react-router-dom';
-import './navbar.css'
+import './navbar.scss'
 
 function Navbar() {
   const [click, setClick] = useState(false);
@@ -20,6 +20,16 @@ function Navbar() {
     setClick(false);
   }
 
+  function MenuItem(props){
+    return (
+      <li className='nav-item'>
+        <button className='nav-links'onClick={()=>scrollto_div(props.section)}>
+          {props.name}
+        </button>
+      </li>
+    );
+  }
+
   const showButton = () => {
     if (window.innerWidth <= 960) {
       setButton(false);
@@ -34,7 +44,7 @@ function Navbar() {
 
   window.addEventListener('resize', showButton);
   window.addEventListener('scroll', function(){
-    if (window.scrollY > 10){
+    if (window.scrollY > 0){
       setNav_up(false);
     } else {
       setNav_up(true);
@@ -42,51 +52,67 @@ function Navbar() {
   })
 
   return (
-      <nav className={ !click & nav_up ? 'navbar' : 'navbar-up'}>
-        <div className='navbar-container'>
-          <Link to='/' className='navbar-logo' onClick={closeMobileMenu}>
-            GrowSocial
-          </Link>
-          <div className='menu-icon' onClick={handleClick}>
-            <img src={click ? './assets/menu_icon.png' : './assets/menu_icon_whiet.png'} alt="menu" />
-          </div>
-          <ul className={ click ? 'nav-menu active' : 'nav-menu'}>
-            <li className='nav-item'>
-              <a className='nav-links' 
-                onClick={()=>scrollto_div('home')}>
-                Home
-              </a>
-            </li>
-            <li className='nav-item'>
-              <a
-                className='nav-links'
-                onClick={()=>scrollto_div('likesAndfolliwers')}
-              >
-                Get Free Likes
-              </a>
-            </li>
-            <li className='nav-item'>
-              <a
-                className='nav-links'
-                onClick={()=>scrollto_div('likesAndfolliwers')}
-              >
-                Get Free Followers
-              </a>
-            </li>
-            <li className='nav-item'>
-              <a className='nav-links' onClick={()=>scrollto_div('downloadApp')}>
-                Download App
-              </a>
-            </li>
-            <li className='nav-item'>
-              <a className='nav-links' onClick={()=>scrollto_div('contactUs')}>
-                Contact US
-              </a>
-            </li>
-          </ul>
-        </div>
-      </nav>
+    <div className={ !click & nav_up ? 'navbar' : 'navbar-up'}>
+      <span className='logo'><Link to='/' className='navbar-logo' onClick={closeMobileMenu}>GrowSocial</Link></span>
+      <span className='menu-icon' onClick={handleClick}>
+        <img src={ !click & nav_up ? './assets/menu_icon_whiet.png' : './assets/menu_icon.png' } alt="menu" />
+      </span>
+      <ul className={click ? 'nav-menu active' : 'nav-menu'}>
+        <MenuItem name = 'home' section='home' />
+        <MenuItem name = 'Get Free Likes' section='likesAndfolliwers' />
+        <MenuItem name = 'Get Free Followers' section='likesAndfolliwers' />
+        <MenuItem name = 'Download App' section='downloadApp' />
+        <MenuItem name = 'Contact US' section='contactUs' />
+      </ul>
+    </div>
   );
+
+  // return (
+  //     <div className={ !click & nav_up ? 'navbar' : 'navbar-up'}>
+  //       <div className='navbar-container'>
+  //         <Link to='/' className='navbar-logo' onClick={closeMobileMenu}>
+  //           GrowSocial
+  //         </Link>
+  //         <div className='menu-icon' onClick={handleClick}>
+  //           <img src={nav_up ? './assets/menu_icon_whiet.png' : './assets/menu_icon.png' } alt="menu" />
+  //         </div>
+  //         <ul className={ click ? 'nav-menu active' : 'nav-menu'}>
+  //           <li className='nav-item'>
+  //             <a className='nav-links' 
+  //               onClick={()=>scrollto_div('home')}>
+  //               Home
+  //             </a>
+  //           </li>
+//             <li className='nav-item'>
+//               <a
+//                 className='nav-links'
+//                 onClick={()=>scrollto_div('likesAndfolliwers')}
+//               >
+//                 Get Free Likes
+//               </a>
+//             </li>
+//             <li className='nav-item'>
+//               <a
+//                 className='nav-links'
+//                 onClick={()=>scrollto_div('likesAndfolliwers')}
+//               >
+//                 Get Free Followers
+//               </a>
+//             </li>
+//             <li className='nav-item'>
+//               <a className='nav-links' onClick={()=>scrollto_div('downloadApp')}>
+//                 Download App
+//               </a>
+//             </li>
+//             <li className='nav-item'>
+//               <a className='nav-links' onClick={()=>scrollto_div('contactUs')}>
+//                 Contact US
+//               </a>
+//             </li>
+//           </ul>
+//         </div>
+//       </div>
+//   );
 }
 
 export default Navbar;
--- src/pages/homePage/components/contactUs.tsx
+++ src/pages/homePage/components/contactUs.tsx
@@ -8,10 +8,14 @@ export default class ContactUS extends React.Component{
             <div className='contactUs' id='contactUs'>
                 <h1>Contact US</h1>
                 <div className='contactUsWraper'>
-                    <ContactCard imageName='./assets/call.svg' heading='Call' desc='Call our sales team' buttonName='Call' />
-                    <ContactCard imageName='./assets/WA.svg' heading='whatsapp' desc='Message us on Whatsapp' buttonName='whatsapp' />
-                    <ContactCard imageName='./assets/telegram.svg' heading='Telegram' desc='Contact us on Telegram' buttonName='whatsapp' />
-                    <ContactCard imageName='./assets/message.svg' heading='Message' desc='Drop a message now' buttonName='whatsapp' />                    
+                    <div className='breakContact'>
+                        <ContactCard imageName='./assets/call.svg' heading='Call' desc='Call our sales team' buttonName='Call' />
+                        <ContactCard imageName='./assets/WA.svg' heading='whatsapp' desc='Message us on Whatsapp' buttonName='whatsapp' />
+                    </div>
+                    <div className='breakContact'>
+                        <ContactCard imageName='./assets/telegram.svg' heading='Telegram' desc='Contact us on Telegram' buttonName='Telegram' />
+                        <ContactCard imageName='./assets/message.svg' heading='Message' desc='Drop a message now' buttonName='Message' />                 
+                    </div>   
                 </div>
             </div>
         );
